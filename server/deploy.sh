e1e675f8e6a5508b104b61796cdafc38bb8a28b7
Nils Ohlmeier <nils@mozilla.com>
2014-07-07 11:53:12 -0700

    avoid git pull from a script which runs from that dir

--- server/deploy.sh
+++ server/deploy.sh
@@ -7,9 +7,6 @@ MOZWEBRTC=$HOME/mozwebrtc
 AUT_OVERLAY=$HOME/aut-overlay
 TESTS_OVERLAY=$HOME/tests-overlay
 
-cd $MOZWEBRTC
-git pull
-
-cd server
+cd $MOZWEBRTC/server
 cp -r aut-overlay/* $AUT_OVERLAY
 cp -r tests-overlay/* $TESTS_OVERLAY
